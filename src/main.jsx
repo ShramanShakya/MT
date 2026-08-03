729370ad2fb7700c8c4434530af943c29844e9e9
ShramanShakya <u6712145@au.edu>
2026-08-03 18:09:49 +0700

    ok

---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { HashRouter, Routes, Route } from "react-router-dom";
+import Page1 from './components/Page1.jsx';
+import Page2 from './components/Page2.jsx';
+import Page3 from './components/Page3.jsx';
+import Page4 from './components/Page4.jsx';
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/page1" element={<Page1 />} />
+        <Route path="/page2" element={<Page2 />} />
+        <Route path="/page3" element={<Page3 />} />
+        <Route path="/page4" element={<Page4 />} />
+      </Routes>
+    </HashRouter>
+  </StrictMode>,
+)
